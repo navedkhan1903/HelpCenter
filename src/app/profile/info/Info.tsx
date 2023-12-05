11a66eac6e5038d3a9b0e6a579dfe4c735e82c2c
Naved Khan <navedkhan19032002@gmail.com>
2023-12-05 21:44:25 +0530

    Loading test

--- src/app/profile/info/Info.tsx
+++ src/app/profile/info/Info.tsx
@@ -21,7 +21,97 @@ declare module "next-auth" {
 }
 
 export default async function Info() {
-  const res = await axios.get("https://dummyjson.com/products");
+  const session = useSession();
 
-  return <p>Hello</p>;
+  const [disabled, setDisabled] = useState(true);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [newPass, setNewPass] = useState("");
+
+  useEffect(() => {
+    if (session.data) {
+      setName(session.data.user?.name!);
+      setEmail(session.data.user?.email!);
+      setPass("");
+      setNewPass("");
+    }
+  }, [session, disabled]);
+
+  async function saveEdit() {
+    try {
+      if (session.data) {
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPass = await bcryptjs.hash(newPass, salt);
+
+        await axios.post("/api/updateInfo", {
+          id: session.data.user._id,
+          name,
+          email,
+          pass: hashedPass,
+        });
+
+        const res = await signIn("credentials", {
+          email,
+          pass: newPass,
+          redirect: false,
+        });
+        if (res!.error) throw new Error(res!.error);
+
+        setDisabled(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <>
+      {session.data && (
+        <Wrapper
+          label="Personal Information"
+          onEdit={() => setDisabled(false)}
+          onSave={saveEdit}
+          onCancel={() => setDisabled(true)}
+          disabled={disabled}
+        >
+          <>
+            <div className="flex justify-between">
+              <InfoInput
+                label="Full Name"
+                type="text"
+                value={disabled ? session.data.user?.name! : name}
+                onChange={(event) => setName(event.target.value)}
+                disabled={disabled}
+              />
+              <InfoInput
+                label="Email Address"
+                type="email"
+                value={disabled ? session.data.user?.email! : email}
+                onChange={(event) => setEmail(event.target.value)}
+                disabled={disabled}
+              />
+            </div>
+
+            <div className="flex justify-between">
+              <InfoInput
+                label="Password"
+                type="password"
+                value={pass}
+                onChange={(event) => setPass(event.target.value)}
+                disabled={disabled}
+              />
+              <InfoInput
+                label="New Password"
+                type="password"
+                value={newPass}
+                onChange={(event) => setNewPass(event.target.value)}
+                disabled={disabled}
+              />
+            </div>
+          </>
+        </Wrapper>
+      )}
+    </>
+  );
 }
