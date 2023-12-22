a23e89949159af8dd85fb4ae46acd45da85fd80a
Naved Khan <navedkhan19032002@gmail.com>
2023-12-22 18:15:01 +0530

    Delpoy Fix

--- src/app/main/testimonials/Testimonials.tsx
+++ src/app/main/testimonials/Testimonials.tsx
@@ -2,10 +2,10 @@ import Reveal from "@/components/Reveal";
 import Testimonial from "./Testimonial";
 import SectionHeading from "@/components/SectionHeading";
 
-export default function Testimonials() {
-  // const res = await fetch(`${process.env.URL}/api/testimonials`, {
-  //   cache: "no-cache",
-  // }).then((res) => res.json());
+export default async function Testimonials() {
+  const res = await fetch(`${process.env.URL}/api/testimonials`, {
+    cache: "no-cache",
+  }).then((res) => res.json());
 
   return (
     <div className="mx-auto mb-40 w-10/12">
@@ -15,7 +15,7 @@ export default function Testimonials() {
           subTitle="Explore what people say about us"
         />
       </Reveal>
-      {/* <Testimonial res={res} /> */}
+      <Testimonial res={res} />
     </div>
   );
 }
