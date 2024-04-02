d1650e0f9c45ee097e4c6d519a47057d380a749e
Naved Khan <navedkhan19032002@gmail.com>
2024-04-03 00:40:04 +0530

    Comment Testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <Numbers />
       <Features />
       <Services />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <FAQ />
       <NewsLetter />
       <Footer />
